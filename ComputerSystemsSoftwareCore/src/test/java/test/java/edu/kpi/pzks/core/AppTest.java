36eaed75a5070003744eddfd4e0be31e09c11d6d
Anastasiia Smirnova <asmirnova@playtika.com.ua>
2013-02-07 16:24:43 +0200

    Add test folder. Change POM, gitignore

---
+++ ComputerSystemsSoftwareCore/src/test/java/test/java/edu/kpi/pzks/core/AppTest.java
@@ -0,0 +1,39 @@
+package test.java.edu.kpi.pzks.core;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author asmirnova
+ */
+public class AppTest {
+    
+    public AppTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void test() {
+        fail("The test case is a prototype.");
+    }
+}
