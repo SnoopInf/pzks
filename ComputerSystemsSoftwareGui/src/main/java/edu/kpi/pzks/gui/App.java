daac5227889eeb4e36f42973f482019f4ada230e
Anastasiia Smirnova <asmirnova@playtika.com.ua>
2013-02-07 16:23:55 +0200

    Add GUI project.

---
+++ ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/App.java
@@ -0,0 +1,13 @@
+package edu.kpi.pzks.gui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
