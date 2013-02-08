0da9cb121d7b06c187ec85f03a2edbd29b6a6250
asmirnova <asmirnova@playtika.com.ua>
2013-02-09 00:55:14 +0200

    Update constants

--- ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/utils/CONSTANTS.java
+++ ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/utils/CONSTANTS.java
@@ -14,6 +14,8 @@ public class CONSTANTS {
     public static final Color BORDER_SELECTED_COLOR = new Color(126, 191, 255);
     public static final Color BORDER_COLOR = Color.BLACK;
     public static final Color DEFAULT_COLOR = Color.BLACK;
+    
+    public static Color GRID_COLOR = new Color(240, 240, 240);
 
     public static final int NODE_WIDTH = 30;
     public static final int NODE_HEIGHT = 30;
