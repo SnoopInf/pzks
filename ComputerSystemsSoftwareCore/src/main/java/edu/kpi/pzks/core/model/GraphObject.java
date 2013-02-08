9a2d7c01271bf3011ab0cd33e89f019ce1fce47e
Anastasiia Smirnova <asmirnova@playtika.com.ua>
2013-02-08 18:31:25 +0200

    Revert "Clear comments inside GraphObject"
    This reverts commit 217dde54805fcc061033890579bf3c9f56c18c98.

--- ComputerSystemsSoftwareCore/src/main/java/edu/kpi/pzks/core/model/GraphObject.java
+++ ComputerSystemsSoftwareCore/src/main/java/edu/kpi/pzks/core/model/GraphObject.java
@@ -7,4 +7,45 @@ import java.io.Serializable;
  * @author Aloren
  */
 public interface GraphObject extends Serializable {
+//TODO please move me to gui
+//    protected boolean selected = false;
+//    protected int weight;
+
+//    public int getWeight() {
+//        return weight;
+//    }
+//
+//    public void setWeight(int weight) {
+//        this.weight = weight;
+//    }
+    
+//    public void setSelected(boolean selected) {
+//        this.selected = selected;
+//    }
+//
+//    public boolean isSelected() {
+//        return this.selected;
+//    }
+
+//    public abstract String getName();
+
+//    public abstract int getX();
+
+//    public abstract int getY();
+
+//    public abstract int getWidth();
+
+//    public abstract int getHeight();
+
+//    public abstract void setPoint(int x, int y);
+
+//    public abstract void paintComponent(Graphics2D g);
+
+//    public abstract void setPoint(Point2D.Double p);
+
+//    public abstract Point2D.Double getPoint();
+
+//    public abstract boolean contains(int x, int y);
+
+//    public abstract JPopupMenu getPopup(Graph graph);
 }
