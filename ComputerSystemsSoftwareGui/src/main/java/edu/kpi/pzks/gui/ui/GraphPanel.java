763b2da933f2323e3f6979dcfdac3e18351fa0dc
asmirnova <asmirnova@playtika.com.ua>
2013-02-10 16:56:04 +0200

    Add selectedNodes and checkSize method to GraphPanel

--- ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/ui/GraphPanel.java
+++ ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/ui/GraphPanel.java
@@ -2,13 +2,15 @@ package edu.kpi.pzks.gui.ui;
 
 import edu.kpi.pzks.core.model.Graph;
 import edu.kpi.pzks.gui.modelview.GraphView;
+import edu.kpi.pzks.gui.modelview.NodeView;
 import edu.kpi.pzks.gui.modelview.impl.GraphViewImpl;
-import edu.kpi.pzks.gui.ui.tools.SelectionDraggingTool;
 import edu.kpi.pzks.gui.ui.tools.Tool;
 import edu.kpi.pzks.gui.ui.utils.Grid;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.RenderingHints;
 import java.util.HashSet;
 import java.util.Set;
@@ -23,6 +25,7 @@ public class GraphPanel extends JPanel {
     private Set<Tool> currentTools = new HashSet<>();
     private Grid grid = new Grid();
     private GraphView graphView = new GraphViewImpl(new Graph());
+    private Set<NodeView> selectedNodeViews = new HashSet<>();
 
     public GraphPanel() {
         setBackground(Color.WHITE);
@@ -38,7 +41,7 @@ public class GraphPanel extends JPanel {
                 RenderingHints.VALUE_ANTIALIAS_ON);
         grid.paint(g2, getWidth(), getHeight());
         graphView.paint(g2);
-        for(Tool selectionDraggingTool : currentTools) {
+        for (Tool selectionDraggingTool : currentTools) {
             selectionDraggingTool.paint(g2);
         }
     }
@@ -56,11 +59,11 @@ public class GraphPanel extends JPanel {
         currentTools.add(tool);
         addToolAsMouseListener(tool);
     }
-    
+
     public void setCurrentTools(Set<Tool> tools) {
         clearAllTools();
         currentTools.addAll(tools);
-        for(Tool tool : tools) {
+        for (Tool tool : tools) {
             addToolAsMouseListener(tool);
         }
     }
@@ -75,10 +78,37 @@ public class GraphPanel extends JPanel {
     }
 
     private void clearAllTools() {
-        for(Tool tool : currentTools) {
+        for (Tool tool : currentTools) {
             this.removeMouseListener(tool);
             this.removeMouseMotionListener(tool);
         }
         this.currentTools.clear();
+    }
+
+    public void addSelectedNodeView(NodeView nodeView) {
+        this.selectedNodeViews.add(nodeView);
+    }
+
+    public Set<NodeView> getSelectedNodeViews() {
+        return selectedNodeViews;
+    }
+
+    public void setSelectedNodeViews(Set<NodeView> selectedNodeViews) {
+        this.selectedNodeViews = selectedNodeViews;
+    }
+
+    public void checkSize() {
+        Dimension dimension = new Dimension(0, 0);
+        for (NodeView nodeView : graphView.getNodeViews()) {
+            Point point = nodeView.getUpperLeftCorner();
+            if ((point.x + nodeView.getWidth()) > dimension.width) {
+                dimension.width = point.x + nodeView.getWidth() + 10;
+            }
+            if ((point.y + nodeView.getHeight()) > dimension.height) {
+                dimension.height = point.y + nodeView.getHeight() + 10;
+            }
+        }
+        this.setSize(dimension);
+        this.setPreferredSize(dimension);
     }
 }
