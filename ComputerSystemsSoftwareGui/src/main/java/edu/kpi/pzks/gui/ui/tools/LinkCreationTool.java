f3b242a0b40ed16b6d4f056fec79d8749563b3ef
asmirnova <asmirnova@playtika.com.ua>
2013-02-10 23:47:03 +0200

    Add LinkCreationTool realization

--- ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/ui/tools/LinkCreationTool.java
+++ ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/ui/tools/LinkCreationTool.java
@@ -1,8 +1,17 @@
 package edu.kpi.pzks.gui.ui.tools;
 
+import edu.kpi.pzks.core.model.Link;
+import edu.kpi.pzks.gui.modelview.LinkView;
+import edu.kpi.pzks.gui.modelview.NodeView;
+import edu.kpi.pzks.gui.modelview.impl.LinkViewImpl;
 import edu.kpi.pzks.gui.ui.GraphPanel;
+import edu.kpi.pzks.gui.utils.COLORS;
+import edu.kpi.pzks.gui.utils.PaintUtils;
+import java.awt.BasicStroke;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Point2D;
 
 /**
  *
@@ -10,20 +19,45 @@ import java.awt.event.MouseEvent;
  */
 public class LinkCreationTool implements Tool {
 
+    private Point mousePosition;
+    private boolean needToPaint;
     private final GraphPanel graphPanel;
+    private LinkView activeLinkView;
 
     public LinkCreationTool(GraphPanel graphPanel) {
         this.graphPanel = graphPanel;
+        this.mousePosition = new Point(0, 0);
     }
 
     @Override
     public void paint(Graphics2D g2) {
-//        System.out.println("PAINT");
+        if (needToPaint) {
+            paintLink(g2);
+        }
     }
 
     @Override
     public void mouseClicked(MouseEvent me) {
-        System.out.println("Mouse clicked");
+        NodeView nodeView = graphPanel.getGraphView().getNodeViewAtPoint(me.getX(), me.getY());
+        if (nodeView != null) {
+            if (graphPanel.getSelectedNodeViews().isEmpty()) { //first click
+                graphPanel.getSelectedNodeViews().add(nodeView);
+                activeLinkView = new LinkViewImpl(nodeView, null, null);
+            } else { //second click
+                if (activeLinkView != null) {
+                    activeLinkView.setToNodeView(nodeView);
+                    activeLinkView.setLink(new Link(activeLinkView.getFromNodeView().getNode(), nodeView.getNode()));
+                    graphPanel.getGraphView().addLinkView(activeLinkView);
+                    graphPanel.getSelectedNodeViews().clear();
+                    removeActiveLinkView();
+                }
+            }
+        }
+        graphPanel.repaint();
+    }
+
+    public void removeActiveLinkView() {
+        activeLinkView = null;
     }
 
     @Override
@@ -36,19 +70,43 @@ public class LinkCreationTool implements Tool {
 
     @Override
     public void mouseEntered(MouseEvent me) {
+        needToPaint = true;
     }
 
     @Override
     public void mouseExited(MouseEvent me) {
+        needToPaint = false;
+        activeLinkView = null;
+        graphPanel.getSelectedNodeViews().clear();
+        graphPanel.repaint();
     }
 
     @Override
     public void mouseDragged(MouseEvent me) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void mouseMoved(MouseEvent me) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (needToPaint) {
+            mousePosition = new Point(me.getX(), me.getY());
+            graphPanel.repaint();
+        }
+    }
+
+    private void paintLink(Graphics2D g2) {
+        if (activeLinkView != null) {
+            g2.setColor(COLORS.LINK_COLOR);
+            g2.setStroke(new BasicStroke(1.5f));
+            final NodeView fromNodeView = activeLinkView.getFromNodeView();
+            final Point fromNodeViewCenter = fromNodeView.getCenter();
+            double dx = PaintUtils.getLength(fromNodeViewCenter.x, mousePosition.x);
+            double dy = PaintUtils.getLength(fromNodeViewCenter.y, mousePosition.y);
+            double theta = Math.atan2(dy, dx);
+            Point2D.Double intersectionPoint = PaintUtils.getEllipseIntersectionPoint(theta, 
+                    fromNodeView.getWidth(), fromNodeView.getHeight());
+            g2.drawLine((int) (fromNodeViewCenter.x + intersectionPoint.getX()), 
+                    (int) (fromNodeViewCenter.y + intersectionPoint.getY()),
+                    mousePosition.x, mousePosition.y);
+        }
     }
 }
