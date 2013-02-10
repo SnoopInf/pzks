8e7c263a7aa0aded15ae5a52facadae4b6278db3
asmirnova <asmirnova@playtika.com.ua>
2013-02-10 17:42:42 +0200

    Remove UnsupportedOperationException

--- ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/ui/tools/LinkDraggingTool.java
+++ ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/ui/tools/LinkDraggingTool.java
@@ -18,41 +18,33 @@ public class LinkDraggingTool implements SelectionDraggingTool {
 
     @Override
     public void paint(Graphics2D g2) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void mouseClicked(MouseEvent me) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void mousePressed(MouseEvent me) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void mouseReleased(MouseEvent me) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void mouseEntered(MouseEvent me) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void mouseExited(MouseEvent me) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void mouseDragged(MouseEvent me) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void mouseMoved(MouseEvent me) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
