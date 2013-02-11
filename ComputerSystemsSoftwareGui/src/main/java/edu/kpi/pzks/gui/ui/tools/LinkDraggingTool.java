49747448b894558698a543aa36cce29799272239
Anastasiia Smirnova <asmirnova@playtika.com.ua>
2013-02-11 17:56:16 +0200

    Add dragging for bend point. Contains fooocode. Sorry:)

--- ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/ui/tools/LinkDraggingTool.java
+++ ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/ui/tools/LinkDraggingTool.java
@@ -1,19 +1,23 @@
 package edu.kpi.pzks.gui.ui.tools;
 
 import edu.kpi.pzks.gui.modelview.LinkView;
+import edu.kpi.pzks.gui.modelview.NodeView;
 import edu.kpi.pzks.gui.ui.GraphPanel;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
 
 /**
- *
+ * Lot of fooooo code. Sorry
+ * TODO refactor!!!
  * @author Aloren
  */
 public class LinkDraggingTool implements SelectionDraggingTool {
 
     private final GraphPanel graphPanel;
-//    private Point mousePosition = new Point(0, 0);
+    private Point mousePosition = new Point(0, 0);
     private LinkView selectedLinkView;
+    private NodeView selectedNodeView;
 
     public LinkDraggingTool(GraphPanel graphPanel) {
         this.graphPanel = graphPanel;
@@ -32,11 +36,15 @@ public class LinkDraggingTool implements SelectionDraggingTool {
 
     @Override
     public void mousePressed(MouseEvent me) {
+        if (graphPanel.getGraphView() != null) {
+            beginDragging(me);
+        }
     }
 
     @Override
     public void mouseReleased(MouseEvent me) {
         selectedLinkView = null;
+        selectedNodeView = null;
     }
 
     @Override
@@ -49,18 +57,42 @@ public class LinkDraggingTool implements SelectionDraggingTool {
 
     @Override
     public void mouseDragged(MouseEvent me) {
-        //TODO update drag bendPoint for multiple links
-        if (graphPanel.getSelectedLinkViews().contains(selectedLinkView)) {
+        if (graphPanel.getSelectedLinkViews().contains(selectedLinkView)
+                && graphPanel.getSelectedLinkViews().size() == 1) {
             for (LinkView linkView : graphPanel.getSelectedLinkViews()) {
                 linkView.setBendPoint(me.getX(), me.getY());
             }
             graphPanel.checkSize();
             graphPanel.repaint();
+        } else if (graphPanel.getSelectedNodeViews().contains(selectedNodeView)) {
+            Point pp = graphPanel.getGrid().getSnapToGridPoint(me.getX(), me.getY());
+            int dx = (int) (pp.x - mousePosition.x);
+            int dy = (int) (pp.y - mousePosition.y);
+            for (LinkView linkView : graphPanel.getSelectedLinkViews()) {
+                if (linkView.hasBendPoint()) {
+                    Point bP = linkView.getBendPoint();
+                    linkView.setBendPoint((int) bP.getX() + dx, (int) bP.getY() + dy);
+                }
+            }
+            mousePosition.x += dx;
+            mousePosition.y += dy;
+            graphPanel.checkSize();
+            graphPanel.repaint();
         }
+
     }
 
     @Override
     public void mouseMoved(MouseEvent me) {
-//        mousePosition = graphPanel.getGrid().getSnapToGridPoint(me.getX(), me.getY());
+        mousePosition = graphPanel.getGrid().getSnapToGridPoint(me.getX(), me.getY());
+    }
+
+    private void beginDragging(MouseEvent me) {
+        int x = me.getX();
+        int y = me.getY();
+        selectedNodeView = graphPanel.getGraphView().getNodeViewAtPoint(x, y);
+        if (graphPanel.getSelectedNodeViews().contains(selectedNodeView)) {
+            mousePosition = new Point(x, y);
+        }
     }
 }
