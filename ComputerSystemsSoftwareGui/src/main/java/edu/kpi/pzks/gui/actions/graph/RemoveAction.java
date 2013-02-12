3bf1acc8143f4e053598d5d5c338c29d953b4275
Anastasiia Smirnova <asmirnova@playtika.com.ua>
2013-02-12 11:52:46 +0200

    Add remove tool by click

---
+++ ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/actions/graph/RemoveAction.java
@@ -0,0 +1,29 @@
+package edu.kpi.pzks.gui.actions.graph;
+
+import edu.kpi.pzks.gui.ui.MainFrame;
+import edu.kpi.pzks.gui.ui.tools.RemoveTool;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+
+/**
+ *
+ * @author asmirnova
+ */
+public class RemoveAction extends AbstractAction {
+
+    private MainFrame mainFrame;
+    private final RemoveTool taskRemoveTool;
+    private final RemoveTool systemRemoveTool;
+
+    public RemoveAction(MainFrame mainFrame) {
+        this.mainFrame = mainFrame;
+        this.taskRemoveTool = new RemoveTool(mainFrame.getTaskPanel());
+        this.systemRemoveTool = new RemoveTool(mainFrame.getSystemPanel());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        mainFrame.getTaskPanel().setCurrentTool(taskRemoveTool);
+        mainFrame.getSystemPanel().setCurrentTool(systemRemoveTool);
+    }
+}
