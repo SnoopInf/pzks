09883c184ae9273b7d4250ae686cedf7464151e3
Anastasiia Smirnova <asmirnova@playtika.com.ua>
2013-02-12 11:54:14 +0200

    Add remove button to ui

--- ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/ui/MainFrame.java
+++ ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/ui/MainFrame.java
@@ -4,6 +4,7 @@ import edu.kpi.pzks.core.validator.ConsistencyValidator;
 import edu.kpi.pzks.core.validator.CyclingValidator;
 import edu.kpi.pzks.gui.actions.graph.LinkCreationToolAction;
 import edu.kpi.pzks.gui.actions.graph.NodeCreationToolAction;
+import edu.kpi.pzks.gui.actions.graph.RemoveAction;
 import edu.kpi.pzks.gui.actions.graph.SelectionDraggingToolAction;
 import edu.kpi.pzks.gui.actions.ui.OpenAction;
 import edu.kpi.pzks.gui.actions.ui.SaveAsAction;
@@ -118,6 +119,10 @@ public class MainFrame extends JFrame {
         JButton selectButton = new JButton(new SelectionDraggingToolAction(this));
         selectButton.setIcon(selectIcon);
 
+        ImageIcon removeIcon = new ImageIcon(getClass().getResource(iconsPath + "/remove.png"));
+        JButton removeButton = new JButton(new RemoveAction(this));
+        removeButton.setIcon(removeIcon);
+
         toolBar.add(openButton);
         toolBar.add(saveButton);
         toolBar.add(genTaskGraphButton);
@@ -125,6 +130,7 @@ public class MainFrame extends JFrame {
         toolBar.add(newNodeButton);
         toolBar.add(newLinkButton);
         toolBar.add(selectButton);
+        toolBar.add(removeButton);
 
         return toolBar;
     }
