f2fb8b556d575a92589c2005e7e42d1c8d5ce7a4
Andrew Berezovskiy <andrew@berezovskiy.org.ua>
2013-02-13 23:04:38 +0200

    Add custom-skinned MainFrame implementation.

---
+++ ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/ui/MainSmarxFrame.java
@@ -0,0 +1,39 @@
+package edu.kpi.pzks.gui.ui;
+
+import com.sun.java.swing.plaf.windows.WindowsTabbedPaneUI;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import javax.swing.*;
+
+/**
+ *
+ * @author Andrew
+ */
+public class MainSmarxFrame extends MainFrame {
+
+    public MainSmarxFrame(String title) {
+        this(title, true);
+    }
+
+    public MainSmarxFrame(String title, boolean init) {
+        super(title);
+        TOOLBAR_PLACEMENT = BorderLayout.WEST;
+        TOOLBAR_ORIENTATION = JToolBar.VERTICAL;
+        initFrame();
+    }
+
+    @Override
+    protected Container getMainPane(JPanel taskPanel, JPanel systemPanel) {
+        JTabbedPane pane = new JTabbedPane();
+        pane.setUI(new WindowsTabbedPaneUI());
+
+        JScrollPane taskPane = new JScrollPane();
+        taskPane.setViewportView(taskPanel);
+        pane.add("PROBLEM", taskPane);
+
+        JScrollPane systemPane = new JScrollPane();
+        systemPane.setViewportView(systemPanel);
+        pane.add("SYSTEM", systemPane);
+        return pane;
+    }
+}
