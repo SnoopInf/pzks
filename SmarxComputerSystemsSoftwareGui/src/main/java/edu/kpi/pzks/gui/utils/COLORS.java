c14750c29c27f6ab65db4145c1a7c1737db45ed5
Andrew Berezovskiy <andrew@berezovskiy.org.ua>
2013-02-21 18:45:14 +0200

    Add custom project configuration

---
+++ SmarxComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/utils/COLORS.java
@@ -0,0 +1,28 @@
+package edu.kpi.pzks.gui.utils;
+
+import java.awt.*;
+import java.util.ResourceBundle;
+
+public class COLORS {
+
+    private static final ResourceBundle colors = Resources.colors;
+
+    public static final Color DEFAULT_COLOR = Color.decode(colors.getString("color.default"));
+
+    public static final Color NODE_COLOR = Color.decode(colors.getString("color.node"));
+    public static final Color NODE_TEXT_COLOR = Color.decode(colors.getString("color.node.text"));
+    public static final Color NODE_SELECTED_COLOR = Color.decode(colors.getString("color.fill.blue"));
+    public static final Color NODE_BORDER_COLOR = Color.decode(colors.getString("color.node.border"));
+    public static final Color NODE_BORDER_SELECTED_COLOR = Color.decode(colors.getString("color.node.border.selected"));
+
+    public static final Color LINK_COLOR = Color.decode(colors.getString("color.link"));
+    public static final Color LINK_SELECTED_COLOR = Color.decode(colors.getString("color.link.selected"));
+
+    public static Color BLUE = Color.decode(colors.getString("color.blue"));
+    public static Color GREY = Color.decode(colors.getString("color.grey"));
+    public static Color FILL_BLUE = Color.decode(colors.getString("color.fill.blue"));
+
+    public static final Color GRID_COLOR = Color.decode(colors.getString("color.grid"));
+    public static final Color PANEL_BACKGROUND = Color.decode(colors.getString("color.panel"));
+
+}
