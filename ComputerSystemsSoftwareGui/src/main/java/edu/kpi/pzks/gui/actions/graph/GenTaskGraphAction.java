9190bb59aed44cf1380bc923070b6296db4d7c27
AlorenNet <AlorenNet@AlorenNet-PC>
2013-02-23 15:57:57 +0200

    Change dialog to panel and add raw gui for it

--- ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/actions/graph/GenTaskGraphAction.java
+++ ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/actions/graph/GenTaskGraphAction.java
@@ -7,10 +7,13 @@ import edu.kpi.pzks.gui.modelview.GraphView;
 import edu.kpi.pzks.gui.modelview.impl.GraphViewImpl;
 import edu.kpi.pzks.gui.ui.GraphPanel;
 import edu.kpi.pzks.gui.ui.MainFrame;
+import edu.kpi.pzks.gui.ui.dialogs.GraphGeneratorPanel;
+import edu.kpi.pzks.gui.utils.STRINGS;
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -26,24 +29,24 @@ public class GenTaskGraphAction extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-//        GraphGeneratorDialog dialog = new GraphGeneratorDialog(mainFrame);
-//        dialog.setVisible(true);
-//        int result = JOptionPane.showConfirmDialog(mainFrame, dialog);
-//        if (result == JOptionPane.OK_OPTION) {
-        GraphPanel graphPanel = mainFrame.getTaskPanel();
-        int numberOfNodes = 5;//dialog.getNumberOfNodes();
-        int minNodeWeight = 1;//dialog.getMinNodeWeight();
-        int maxNodeWeight = 5;//dialog.getMaxNodeWeight();
-        double connectivity = 0.5;//dialog.getConnectivity();
-        Graph generatedGraph = GraphFactory.newGraph(numberOfNodes, minNodeWeight, maxNodeWeight, connectivity);
-        GraphView graphView = GraphViewImpl.createView(generatedGraph);
-        Dimension prefSize = graphPanel.getPreferredSize();
-        graphView.setBounds(new Rectangle(0, 0, (int) prefSize.getWidth(), (int) prefSize.getHeight()));
-        graphView.layout(new CircleLayout());
-        graphPanel.setGraphView(graphView);
-        graphPanel.checkGraphIsValid();
-        graphPanel.checkSize();
-        graphPanel.repaint();
-//        }
+        GraphGeneratorPanel dialog = new GraphGeneratorPanel(mainFrame);
+        dialog.setVisible(true);
+        int result = JOptionPane.showConfirmDialog(mainFrame, dialog, STRINGS.GEN_GRAPH, JOptionPane.YES_NO_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            GraphPanel graphPanel = mainFrame.getTaskPanel();
+            int numberOfNodes = dialog.getNumberOfNodes();
+            int minNodeWeight = dialog.getMinNodeWeight();
+            int maxNodeWeight = dialog.getMaxNodeWeight();
+            double connectivity = dialog.getConnectivity();
+            Graph generatedGraph = GraphFactory.newGraph(numberOfNodes, minNodeWeight, maxNodeWeight, connectivity);
+            GraphView graphView = GraphViewImpl.createView(generatedGraph);
+            Dimension prefSize = graphPanel.getPreferredSize();
+            graphView.setBounds(new Rectangle(0, 0, (int) prefSize.getWidth(), (int) prefSize.getHeight()));
+            graphView.layout(new CircleLayout());
+            graphPanel.setGraphView(graphView);
+            graphPanel.checkGraphIsValid();
+            graphPanel.checkSize();
+            graphPanel.repaint();
+        }
     }
 }
--- ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/ui/dialogs/GraphGeneratorPanel.java
+++ ComputerSystemsSoftwareGui/src/main/java/edu/kpi/pzks/gui/ui/dialogs/GraphGeneratorPanel.java
@@ -1,17 +1,13 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.kpi.pzks.gui.ui.dialogs;
 
 import edu.kpi.pzks.gui.ui.MainFrame;
 import edu.kpi.pzks.gui.utils.STRINGS;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.event.ChangeEvent;
@@ -19,45 +15,48 @@ import javax.swing.event.ChangeListener;
 
 /**
  *
- * @author AlorenNet
+ * @author asmirnova
  */
-public class GraphGeneratorDialog extends JDialog {
-
+public class GraphGeneratorPanel extends JPanel {
+    
     private final MainFrame mainFrame;
     private final JSpinner numberOfNodesSpinner;
     private final JSpinner minWeightSpinner;
     private final JSpinner maxWeightSpinner;
     private final JSpinner connectivitySpinner;
-
-    public GraphGeneratorDialog(MainFrame mainFrame) {
-        super(mainFrame, STRINGS.GEN_GRAPH);
+    
+    public GraphGeneratorPanel(MainFrame mainFrame) {
         this.mainFrame = mainFrame;
-
-        JLabel numberOfNodesLabel = new JLabel();
-        JLabel minWeightLabel = new JLabel();
-        JLabel maxWeightLabel = new JLabel();
-        JLabel connectivityLabel = new JLabel();
-
+        JLabel numberOfNodesLabel = new JLabel(STRINGS.NUM_NODES);
+        JLabel minWeightLabel = new JLabel(STRINGS.MIN_WEIGHT);
+        JLabel maxWeightLabel = new JLabel(STRINGS.MAX_WEIGHT);
+        JLabel connectivityLabel = new JLabel(STRINGS.CONNECTIVITY);
+        
         int value = 4;
         int min = 2;
         int max = 50;
         int step = 1;
         SpinnerNumberModel numberOfNodesSpinnerModel = new SpinnerNumberModel(value, min, max, step);
         this.numberOfNodesSpinner = new JSpinner(numberOfNodesSpinnerModel);
-
+        
         int curMinWeight = 1;
         int minWeight = 1;
         int maxWeight = 10;
         final SpinnerNumberModel minWeightSpinnerModel = new SpinnerNumberModel(curMinWeight, minWeight, maxWeight, step);
         this.minWeightSpinner = new JSpinner(minWeightSpinnerModel);
-
+        
         int curMaxWeight = 100;
         SpinnerNumberModel maxWeightSpinnerModel = new SpinnerNumberModel(maxWeight, minWeight, curMaxWeight, step);
         this.maxWeightSpinner = new JSpinner(maxWeightSpinnerModel);
         maxWeightSpinner.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
-                minWeightSpinnerModel.setMaximum((Integer) maxWeightSpinner.getValue());
+                final int curMaxValue = (Integer) maxWeightSpinner.getValue();
+                final int curMinValue = (Integer) minWeightSpinner.getValue();
+                if (curMinValue > curMaxValue) {
+                    minWeightSpinner.setValue(curMaxValue);
+                }
+                minWeightSpinnerModel.setMaximum(curMaxValue);
             }
         });
         
@@ -67,24 +66,41 @@ public class GraphGeneratorDialog extends JDialog {
         double connectivityStep = 0.1;
         SpinnerNumberModel connectivitySpinnerModel = new SpinnerNumberModel(curConnectivity, minConnectivity, maxConnectivity, connectivityStep);
         this.connectivitySpinner = new JSpinner(connectivitySpinnerModel);
-
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        setLocationRelativeTo(mainFrame);
+        
+        setLayout(new GridLayout(3, 1, 1, 1));
+        JPanel numNodesPanel = new JPanel(new GridLayout(1, 2));
+        numNodesPanel.add(numberOfNodesLabel);
+        numNodesPanel.add(numberOfNodesSpinner);
+        add(numNodesPanel);
+        
+        JPanel weightPanel = new JPanel();
+        weightPanel.setBorder(BorderFactory.createTitledBorder(STRINGS.NODE_WEIGHT));
+        weightPanel.setLayout(new GridLayout(2, 2, 10, 10));
+        weightPanel.add(minWeightLabel);
+        weightPanel.add(minWeightSpinner);
+        weightPanel.add(maxWeightLabel);
+        weightPanel.add(maxWeightSpinner);
+        add(weightPanel);
+        
+        JPanel connectivityPanel = new JPanel(new GridLayout(1, 2, 10, 10));
+        connectivityPanel.add(connectivityLabel);
+        connectivityPanel.add(connectivitySpinner);
+        add(connectivityPanel);
     }
-
+    
     public int getNumberOfNodes() {
         return (Integer) numberOfNodesSpinner.getValue();
     }
-
+    
     public int getMinNodeWeight() {
         return (Integer) minWeightSpinner.getValue();
     }
-
+    
     public int getMaxNodeWeight() {
         return (Integer) maxWeightSpinner.getValue();
     }
-
+    
     public double getConnectivity() {
-        return (Integer) connectivitySpinner.getValue();
+        return (Double) connectivitySpinner.getValue();
     }
 }
