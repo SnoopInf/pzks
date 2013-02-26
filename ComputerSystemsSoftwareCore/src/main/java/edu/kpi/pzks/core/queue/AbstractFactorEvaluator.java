f4178d4a1b2ac7ee76b1f37c190396bd983fe35f
Andrew sm@rx Berezovskiy <andrew@berezovskiy.org.ua>
2013-02-26 02:46:23 +0200

    Add abstact interface implementation that handles common operations

---
+++ ComputerSystemsSoftwareCore/src/main/java/edu/kpi/pzks/core/queue/AbstractFactorEvaluator.java
@@ -0,0 +1,63 @@
+package edu.kpi.pzks.core.queue;
+
+import edu.kpi.pzks.core.model.Node;
+
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * @author smarx
+ */
+public abstract class AbstractFactorEvaluator implements FactorEvaluator {
+
+    private Collection<Node> nodes;
+
+    public AbstractFactorEvaluator(Collection<Node> nodes) {
+        this.nodes = nodes;
+    }
+
+    protected boolean isOnCriticalPath(){
+        return false;
+    }
+
+    protected int getCriticalPathForGraph() {
+        return 0;
+    }
+
+    protected int getCriticalPathFromTop(Node n) {
+        return 0;
+    }
+
+    protected int getCriticalPathFromBottom(Node n) {
+        return 0;
+    }
+
+    protected double getWeightedCriticalPathFromTop(Node n) {
+        return (getCriticalPathFromTop(n) + .0d)/getCriticalPathForGraph();
+    }
+
+    protected double getWeightedCriticalPathFromBottom(Node n) {
+        return 0;
+    }
+    
+    protected int getCriticalNumberForGraph() {
+        return 0;
+    }
+
+    protected int getCriticalNumberFromTop(Node n) {
+        return 0;
+    }
+
+    protected int getCriticalNumberFromBottom(Node n) {
+        return 0;
+    }
+
+    protected double getWeightedCriticalNumberFromTop(Node n) {
+        return 0;
+    }
+
+    protected double getWeightedCriticalNumberFromBottom(Node n) {
+        return 0;
+    }
+
+}
