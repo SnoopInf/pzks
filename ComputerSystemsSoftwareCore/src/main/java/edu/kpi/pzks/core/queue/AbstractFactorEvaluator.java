6cce51e5c35211cd07bd228ab454388ccfac8963
Andrew sm@rx Berezovskiy <andrew@berezovskiy.org.ua>
2013-02-26 16:41:46 +0200

    Implement forward critical path calculation

--- ComputerSystemsSoftwareCore/src/main/java/edu/kpi/pzks/core/queue/AbstractFactorEvaluator.java
+++ ComputerSystemsSoftwareCore/src/main/java/edu/kpi/pzks/core/queue/AbstractFactorEvaluator.java
@@ -1,8 +1,11 @@
 package edu.kpi.pzks.core.queue;
 
+import edu.kpi.pzks.core.model.Link;
 import edu.kpi.pzks.core.model.Node;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -11,6 +14,7 @@ import java.util.Set;
 public abstract class AbstractFactorEvaluator implements FactorEvaluator {
 
     private Collection<Node> nodes;
+    private Collection<Link> links;
 
     public AbstractFactorEvaluator(Collection<Node> nodes) {
         this.nodes = nodes;
@@ -21,7 +25,49 @@ public abstract class AbstractFactorEvaluator implements FactorEvaluator {
     }
 
     protected int getCriticalPathForGraph() {
-        return 0;
+
+        int criticalPath;
+        int pathWeight = 0;
+        for(Node n : nodes) {
+            if(n.getOutputNodes().size() == 0) {
+                System.out.printf("Calculate critical path for Node #%d (%d)\n", n.getId(), n.getWeight());
+                List<Node> path = findPathToRoot(n);
+                for (Node pathNode : path) {
+                    System.out.print();
+                }
+                int weight = calculateCriticalPath(path);
+                if(weight > pathWeight) {
+                    pathWeight = weight;
+                }
+            }
+        }
+
+        return pathWeight;
+    }
+
+    private int calculateCriticalPath(List<Node> path) {
+        int weight = 0;
+        for(Node node : path) {
+            weight += node.getWeight();
+        }
+        return weight;
+    }
+
+    protected List<Node> findPathToRoot(Node n) {
+        Collection<Node> inNodes = n.getInputNodes();
+        int criticalWeight = 0;
+        List<Node> criticalPath = new ArrayList<>();
+
+        for(Node current : inNodes) {
+            List<Node> currentPath = findPathToRoot(current);
+            int weight = calculateCriticalPath(currentPath);
+            if(weight > criticalWeight) {
+                criticalPath = currentPath;
+            }
+        }
+
+        criticalPath.add(0, n);
+        return criticalPath;
     }
 
     protected int getCriticalPathFromTop(Node n) {
@@ -58,6 +104,14 @@ public abstract class AbstractFactorEvaluator implements FactorEvaluator {
 
     protected double getWeightedCriticalNumberFromBottom(Node n) {
         return 0;
+    }
+
+    /**
+     * @param n Node which needs to have it's root node retrieved
+     * @return Node which has a simple path to a given node and has no inbound links
+     */
+    protected Node getRootNodeFor(Node n) {
+        return n;
     }
 
 }
