7a3f27e606d20d8abde18c9ba94feee0ac72c04e
Andrew sm@rx Berezovskiy <andrew@berezovskiy.org.ua>
2013-02-26 02:26:38 +0200

    Add queue generator test using Id factor

---
+++ ComputerSystemsSoftwareCore/src/test/java/test/edu/kpi/pzks/core/queue/QueueTest.java
@@ -0,0 +1,44 @@
+package test.edu.kpi.pzks.core.queue;
+
+import edu.kpi.pzks.core.model.Graph;
+import edu.kpi.pzks.core.model.Node;
+import edu.kpi.pzks.core.queue.IdFactorEvaluatorImpl;
+import edu.kpi.pzks.core.queue.Queue;
+import edu.kpi.pzks.core.queue.QueuedNode;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author smarx
+ */
+
+public class QueueTest {
+
+    @Test
+    public void testQueueGenerationOnStaticIdEvaluator() {
+        Graph graph = mock(Graph.class);
+
+        when(graph.getNodes()).thenReturn(new HashSet<>(Arrays.asList(new Node[]{new Node(1, 1),
+                new Node(1, 0), new Node(1, 4)})));
+
+        Queue queue = new Queue(new IdFactorEvaluatorImpl(), graph.getNodes());
+
+        Collection<QueuedNode> queuedNodeCollection = queue.evaluate();
+
+        QueuedNode previous = null;
+        for (QueuedNode current : queuedNodeCollection) {
+            if (previous != null) {
+                assertTrue(current.getQueueingFactor() >= previous.getQueueingFactor());
+                assertTrue(current.getNode().getId() > previous.getNode().getId());
+            }
+            previous = current;
+        }
+    }
+}
