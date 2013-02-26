bc53220bcde5bd0db961cf6eeac500224a510e23
Andrew sm@rx Berezovskiy <andrew@berezovskiy.org.ua>
2013-02-26 02:15:46 +0200

    Add Node wrapper class which has queueing factor field

---
+++ ComputerSystemsSoftwareCore/src/main/java/edu/kpi/pzks/core/queue/QueuedNode.java
@@ -0,0 +1,29 @@
+package edu.kpi.pzks.core.queue;
+
+import edu.kpi.pzks.core.model.Node;
+
+/**
+ * @author smarx
+ */
+public class QueuedNode {
+    private Node node;
+    private double queueingFactor;
+
+    public QueuedNode(Node node, double queueingFactor) {
+        this.node = node;
+        this.queueingFactor = queueingFactor;
+    }
+
+    public Node getNode() {
+        return node;
+    }
+
+    public double getQueueingFactor() {
+        return queueingFactor;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d (%.4f)", node.getId(), queueingFactor);
+    }
+}
