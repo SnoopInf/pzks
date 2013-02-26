5d3d979de744f9a2ffbe5d6a58e1d68f5ff5f2c0
Andrew sm@rx Berezovskiy <andrew@berezovskiy.org.ua>
2013-02-26 02:46:45 +0200

    Task #1 implementation

---
+++ ComputerSystemsSoftwareCore/src/main/java/edu/kpi/pzks/core/queue/WeightedCriticalFactorEvaluatorImpl.java
@@ -0,0 +1,26 @@
+package edu.kpi.pzks.core.queue;
+
+import edu.kpi.pzks.core.model.Node;
+
+import java.util.Collection;
+
+/**
+ * Метод формирования очередей №1
+ *
+ * @author smarx
+ */
+public class WeightedCriticalFactorEvaluatorImpl extends AbstractFactorEvaluator {
+    public WeightedCriticalFactorEvaluatorImpl(Collection<Node> nodes) {
+        super(nodes);
+    }
+
+    @Override
+    public double evaluateFactorForNode(Node node) {
+        return getWeightedCriticalPathFromTop(node) + getWeightedCriticalNumberFromTop(node); // пронормированная сумма
+    }
+
+    @Override
+    public boolean hasNaturalOrder() {
+        return false; // в порядке убывания
+    }
+}
